db8149824636e96e3472d295f9f6869e3d584378
Alexis Bocanegra <alexisbocanegra14@gmail.com>
2023-09-26 19:02:32 -0500

    Fixed spelling and added bootcamp certificate

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-scroll'
+import certificate from '../assets/experience/BootcampCertificate.jpg'
 
 const About = () => {
   return (
@@ -10,16 +11,20 @@ const About = () => {
                     About
                 </p>
             </div>
-            <p className='text-xl mt-20'>
-                A place where I can develop and create software for people to enjoy and utilize, that is where I see myself. My name is <span className='font-bold'>Alexis Bocanegra</span> and I aspire to become an incrediable software developer.
-            </p>
+                <p className='text-xl mt-20'>
+                    A place where I can develop and create software for people to enjoy and utilize, that is where I see myself. My name is <span className='font-bold'>Alexis Bocanegra</span> and I aspire to become an incredible software developer.
+                </p>
 
-            <br />
-        
-            <p className='text-xl'>
-                I recently worked up the courage to begin my coding career through a bootcamp and gained knowledge in various langauges than you can find listed in my <Link to='experience' className='font-bold cursor-pointer underline text-cyan-600'>"Experience"</Link> section. I plan to continue to learn and grow as a developer by tackling new projects or debugging existing ones, no matter the challange. I can honestly say my coding journey has been a blast and I can't wait for what lies ahead. 
+                <br />
+            
+                <p className='text-xl'>
+                    I recently worked up the courage to begin my coding career through a bootcamp and gained knowledge in various languages than you can find listed in my <Link to='experience' className='font-bold cursor-pointer underline text-cyan-600'>"Experience"</Link> section. I plan to continue to learn and grow as a developer by tackling new projects or debugging existing ones, no matter the challenge. I can honestly say my coding journey has been a great experience and I look forward to the opportunities that lie ahead. 
 
-            </p>
+                </p>
+            <div className='flex flex-col justify-center items-center'>
+                <img src={certificate} alt="" className='h-3/4 w-100 rounded-sm flex flex-col justify-center items-center'/>
+            </div>
+
         </div>
     </div>
   )
